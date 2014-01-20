52db2b39060dc95459f7ab1fc6d37f049c3d69c0
AlexTheProgrammer <alexander.business.s@gmail.com>
2014-01-20 20:12:13 +1100

    used an expression

--- src/saygoodnight.rb
+++ src/saygoodnight.rb
@@ -1,7 +1,7 @@
 def say_goodnight(name)
-	result = " Good night, " + name
+	result = " Good night, #{name.capitalize}"  
 	return result 
 end
 #Time for bed...
-puts say_goodnight("John-Boy")
+puts say_goodnight('uncle')
 puts say_goodnight("Mary-Ellen")
