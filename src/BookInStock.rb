8edf5359059fe7fe2dad6a94f10457739484d7e4
AlexTheProgrammer <alexander.business.s@gmail.com>
2014-01-27 19:47:09 +1100

    rendering instance variables

--- src/BookInStock.rb
+++ src/BookInStock.rb
@@ -3,4 +3,13 @@ class BookInStock
 		@isbn = isbn
 		@price = Float(price)
 	end
+	def to_s
+		"ISBN: #{@isbn}, price: #{@price}"
+	end
 end
+b1 = BookInStock.new("isbn1", 3)
+puts b1 
+b2 = BookInStock.new("isbn2", 3.14)
+puts b2
+b3 = BookInStock.new("isbn3", "5.67")
+puts b3
