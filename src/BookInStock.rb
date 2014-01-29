c0072d0ec10b1c7cd02cc0c80db9c0103c18e8f6
AlexTheProgrammer <alexander.business.s@gmail.com>
2014-01-29 20:40:47 +1100

    csv reader

--- src/BookInStock.rb
+++ src/BookInStock.rb
@@ -8,6 +8,12 @@ class BookInStock
 	def to_s
 		"ISBN: #{@isbn}, price: #{@price}"
 	end
+	def price_in_cents
+		Integer(price*100 + 0.5)
+	end
+	def price_in_cents=(cents)
+		@price = cents/100.0
+	end
 end
 b1 = BookInStock.new("isbn1", 3)
 puts b1 
@@ -19,3 +25,7 @@ book = BookInStock.new("isbn4", 12.34)
 puts "ISBN = #{book.isbn}"
 book.price = book.price - 5
 puts book
+puts "Price in Cents = #{book.price_in_cents}"
+book.price_in_cents = 1234
+puts book.price_in_cents
+
