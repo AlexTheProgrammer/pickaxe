3fa69e0ebd183bf91fc9e86fe4575a4af33e852f
AlexTheProgrammer <alexander.business.s@gmail.com>
2014-02-06 23:18:49 +1100

    added a play class to test my Ruby skills so far

---
+++ src/practice_ruby.rb
@@ -0,0 +1,17 @@
+class Monkeys
+	def initialize(size)
+		if size<10
+		       	@monkey_size = "small monkey"
+		else 
+			@monkey_size = "big monkey"
+		end
+	end
+	def monkey_says
+		puts "I'm a small #{@monkey_size}"
+	end
+end
+
+	a = Monkeys.new(2)
+	b = Monkeys.new(30)
+	a.monkey_says
+	b.monkey_says
